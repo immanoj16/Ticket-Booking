9eba26fb8c03c44e9a2515b3bbc73e86c4cbcccd
immanoj16 <mmaharana14@gmail.com>
2017-08-23 17:19:30 +0530

    Add: click event with seat no

--- src/js/components/Seat.js
+++ src/js/components/Seat.js
@@ -1,21 +1,47 @@
 import React, { Component } from 'react'
 
 
-const Row = (props) => {
-  const rows = []
-  for (let i = 0; i < 12; i++) {
-    rows.push(i)
+class Row extends Component {
+  constructor (props) {
+    super (props)
+
+    this.state = {
+      className: null
+    }
+    this.handleSeatColor = this.handleSeatColor.bind(this)
   }
-  return (
-    <tr>
-      <td>{props.alpha}</td>
-      {rows.map(row => {
-        return (
-          <td><div className='seat' /></td>
-        )
-      })}
-    </tr>
-  )
+
+  handleSeatColor (no) {
+
+    if (this.state.className === null) {
+      this.setState({
+        className: 'selected'
+      })
+    } else {
+      this.setState({
+        className: null
+      })
+    }
+    console.log(this.props.alpha + (no + 1))
+  }
+
+  render () {
+    const rows = []
+    for (let i = 0; i < 12; i++) {
+      rows.push(i)
+    }
+
+    return (
+      <tr>
+        <td>{this.props.alpha}</td>
+        {rows.map((row, no) => {
+          return (
+            <td key={row}><div className={`seat ${this.state.className}`} onClick={() => this.handleSeatColor(no)}/></td>
+          )
+        })}
+      </tr>
+    )
+  }
 }
 
 class Seat extends Component {
@@ -23,7 +49,7 @@ class Seat extends Component {
   render () {
     const list = []
     for (let i = 1; i < 13; i++) {
-      list.push(<th>{i}</th>)
+      list.push(<th key={i}>{i}</th>)
     }
 
     const rows = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J']
@@ -38,7 +64,7 @@ class Seat extends Component {
             </tr>
           </thead>
           <tbody>
-          {rows.map(row => <Row alpha={row} />)}
+          {rows.map((row, no) => <Row key={row} alpha={row} />)}
           </tbody>
         </table>
         <button className="btn center-block">Confirm Booking</button>
